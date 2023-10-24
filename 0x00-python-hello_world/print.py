f44f3168f73eadbd83685084ff8c27d85f26fa27
osubaba <austineochij@gmail.com>
2023-10-24 14:20:52 -0700

    c or python

---
+++ 0x00-python-hello_world/print.py
@@ -0,0 +1,10 @@
+# how to print a string
+print("hello, world")
+
+#how to print an integer
+print(7)
+
+#how to print a variable
+my_name = "Austine Codes"
+
+print(my_name)
---
+++ 0x00-python-hello_world/str.py
@@ -0,0 +1,36 @@
+#concanatations in python
+
+my_name = "Austine codes"
+
+#now lets join them using python
+
+print("I loves how " + my_name + " codes using python")
+
+#Again using spaces to understand this better 
+
+my_baby = "baby bali"
+
+print("the best thing that happened to me is" + " " + my_baby + " " + "I love her so much")
+
+#including a comma
+
+God_given = "dad"
+
+print("my",God_given,"is good")
+
+#using curly braces
+
+first_sub = "medical engineering"
+
+print("nyarangi was doing {}, as her course".format(first_sub))
+
+#using fstring to print strings
+singer = "osubaba"
+
+print(f"Burnaboy is coming  alongside, {singer} to kenya")
+
+#using fstring to print two strings
+karao = "police man"
+place = "bamburi"
+
+print(f"the {karao} at {place} is happy" )
